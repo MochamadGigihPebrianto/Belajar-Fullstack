f8809222725b078b2e90dbb3a262a9087e1cb201
Mochamad Gigih Pebrianto <gfreelance14@gmail.com>
2023-02-22 03:54:14 +0700

    menambah spasi

--- master--branch.js
+++ master--branch.js
@@ -1,9 +1,13 @@
 console.info("Berikut ini macam-macam fungsi dalam aritmetika");
 
-function perkalian(a, b) {
-  return a * b;
-}
+
+
+
 
 function pembagian (a,b) {
     return a/b;
 }
+
+function perkalian(a, b) {
+    return a * b;
+  }
